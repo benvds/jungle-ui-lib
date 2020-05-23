715a5d1b1e93a64c1c53780e4b6c2c33b5cb8922
Ben van de Sande <ben@pomodoro.nl>
2020-05-23 15:27:45 +0200

    fix setting correct react externals

--- webpack.config.js
+++ webpack.config.js
@@ -56,8 +56,18 @@ module.exports = webpackEnv => {
       ]
     },
     externals: {
-      react: "React",
-      "react-dom": "ReactDOM"
+      react: {
+        root: "React",
+        commonjs2: "react",
+        commonjs: "react",
+        amd: "react"
+      },
+      "react-dom": {
+        root: "ReactDOM",
+        commonjs2: "react-dom",
+        commonjs: "react-dom",
+        amd: "react-dom"
+      }
     }
   };
 };
